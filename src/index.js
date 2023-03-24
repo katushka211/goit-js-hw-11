3b83ecd547328d8850101b86c1d0f5c05881ea0b
Kateryna Verveda <katushka211@gmail.com>
2023-03-24 14:10:46 +0100

    add changes

--- src/index.js
+++ src/index.js
@@ -8,44 +8,63 @@ const refs = {
   form: document.querySelector('.search-form'),
   input: document.querySelector('input'),
   gallery: document.querySelector('.gallery'),
-  buttonLoadMore: document.querySelector('.load-more'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
 let page = 1;
+refs.loadMoreBtn.style.display = 'none';
 
 refs.form.addEventListener('submit', onSearch);
-// refs.buttonLoadMore.addEventListener('click', onLoadMore);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
 function onSearch(event) {
   event.preventDefault();
   const searchImage = refs.input.value;
   //   console.log(searchImage);
-  refs.input.value = '';
+  refs.gallery.innerHTML = '';
   page = 1;
   if (searchImage) {
     getImages(searchImage);
   } else {
     Notiflix.Notify.info('Please enter a search term');
   }
+}
 
-  async function getImages(searchImage) {
-    try {
-      const API_KEY = '34605013-ed7b70ee5300a8874900b8fd4';
-      const response = await axios.get(
-        `https://pixabay.com/api/?key=${API_KEY}&q=${searchImage}&image_type=photo&orientation=horizontal&safesearch=true`
+async function getImages(searchImage) {
+  try {
+    const API_URL = 'https://pixabay.com/api/';
+    const API_KEY = '34605013-ed7b70ee5300a8874900b8fd4';
+    const params = {
+      key: API_KEY,
+      q: searchImage,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: page,
+    };
+    const response = await axios.get(API_URL, { params });
+    if (response.data.hits.length > 0 && page === 1) {
+      Notiflix.Notify.success(
+        `Hooray! We found ${response.data.totalHits} images.`
       );
-      if (response.data.hits.length > 0) {
-        Notiflix.Notify.success(
-          `Hooray! We found ${response.data.totalHits} images.`
-        );
-      } else {
-        Notiflix.Notify.failure(
-          `"Sorry, there are no images matching your search query. Please try again."`
-        );
-      }
-      createGallery(response.data);
-    } catch (error) {
-      console.log(error);
+      refs.loadMoreBtn.style.display = 'block';
     }
+    if (response.data.hits.length === 0) {
+      Notiflix.Notify.failure(
+        `"Sorry, there are no images matching your search query. Please try again."`
+      );
+    }
+    if (response.data.hits.length < 40) {
+      refs.loadMoreBtn.style.display = 'none';
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+
+    createGallery(response.data);
+    smoothScroll();
+  } catch (error) {
+    console.log(error);
   }
 }
 
@@ -74,4 +93,28 @@ function createGallery(array) {
     )
     .join('');
   refs.gallery.insertAdjacentHTML('beforeend', markup);
+  simpleLightbox.refresh();
+}
+
+const simpleLightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+function onLoadMoreBtnClick(event) {
+  event.preventDefault();
+  const searchImage = refs.input.value;
+  page += 1;
+  getImages(searchImage);
+}
+
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
